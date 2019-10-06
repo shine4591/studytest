24ef721e25cc0e1c0a146ef976cba59036465360
andy <532878631@qq.com>
2019-10-06 08:22:33 +0800

    threadlocal

---
+++ singtonPattern/src/main/java/com/andy/singleton/threadlocal/ThreadLocalSingleton.java
@@ -0,0 +1,17 @@
+package com.andy.singleton.threadlocal;
+
+public class ThreadLocalSingleton {
+    private static final ThreadLocal<ThreadLocalSingleton> threadLocalInstance =
+            new ThreadLocal<ThreadLocalSingleton>(){
+                protected ThreadLocalSingleton initialValue() {
+                    return new ThreadLocalSingleton();
+                }
+            };
+
+    private ThreadLocalSingleton(){}
+
+    public static ThreadLocalSingleton getInstance(){
+        return threadLocalInstance.get();
+    }
+
+}
